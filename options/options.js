8db2e0a936839c9b3b1e9d93e61b389183709d76
Crowdbotics <team@crowdbotics.com>
2024-02-16 03:19:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-bonus-46388",
+  url: "https://fragrant-bonus-46388.botics.co",
+  api: "https://fragrant-bonus-46388.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
